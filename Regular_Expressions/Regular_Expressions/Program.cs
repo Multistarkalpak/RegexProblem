dfa2c1f4371b620707723ea9809d4c1b1d21443a
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-28 15:10:59 +0530

    Writing First name

---
+++ Regular_Expressions/Regular_Expressions/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //uc1
+            RegexClass regex = new RegexClass();
+            regex.ValidateFirstNameRegex("Kalpak");
+
+
+        }
+    }
+}
---
+++ Regular_Expressions/Regular_Expressions/User_Registration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class RegexClass
+    {
+        public Regex FirstNameRegex = new Regex("^[A-Z]{1}[A-Za-z]{3,}?$");
+        public void ValidateFirstNameRegex(string FirstName)
+        {
+            Console.WriteLine("\nFirstName:" + FirstName);
+            if (FirstNameRegex.IsMatch(FirstName))
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("not valid");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+        
+
+
+
+    
+
